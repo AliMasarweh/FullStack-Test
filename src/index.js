4b4667b3281743ea2ab4744371a23bfd315585f9
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2019-12-27 14:39:27 +0200

    added button and input text, have some problems with validations

--- src/index.js
+++ src/index.js
@@ -5,20 +5,56 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hobbies: ["Drawing", "Writing", "Surfing", "Eating", "Coding"]
+      hobbies: ["Drawing", "Writing", "Surfing", "Eating", "Coding"],
+      input: "",
+      errors: []
     };
   }
 
   render() {
     return (
-      <div className="row">
-        <div className="col-6">
-          {this.state.hobbies.map(hobby => (
-            <h1>{hobby}</h1>
+      <>
+        <div className="row">
+          <div className="col-6">
+            {this.state.hobbies.map(hobby => (
+              <h1>{hobby}</h1>
+            ))}
+          </div>
+          <div className="col-6">rightSide</div>
+        </div>
+        <div>
+          <input
+            type="text"
+            onChange={e => (this.state.input = e.target.value)}
+          ></input>
+          <input
+            value="add hobby"
+            type="button"
+            onClick={e => {
+              const value = this.state.input;
+              const errors = [];
+              if (value) {
+                errors.push("empty input");
+              } else if (this.state.hobbies.includes(value)) {
+                errors.push("already contains this hobby!");
+              }
+              if (errors)
+                this.setState({
+                  errors: errors
+                });
+              else
+                this.setState({
+                  hobbies: [value].concat(this.state.hobbies)
+                });
+            }}
+          ></input>
+          {this.state.errors.map((error, index) => (
+            <small key={index} className="form-text text-danger">
+              {error}
+            </small>
           ))}
         </div>
-        <div className="col-6">left side</div>
-      </div>
+      </>
     );
   }
 }
