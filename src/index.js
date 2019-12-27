a3337f0e99caa4ee63cbc30f8cdd014d6e57493c
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2019-12-27 14:16:17 +0200

    added index and some other files, don't have enough time to work on gitignore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+class App extends React.Component {
+  // constructor(props){
+  //     super(props);
+  // }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  render() {
+    return (
+      <div className="row">
+        <div className="col-6">right side</div>
+        <div className="col-6">left side</div>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
